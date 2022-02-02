4e5a193453c7524dca37d5899505c3c7163554e7
Jens Klimke <jens.klimke@rwth-aachen.de>
2022-02-02 15:58:38 +0100

    deploy action added

--- jenkins/write_env.sh
+++
@@ -1,6 +0,0 @@
-echo "REACT_APP_BUILD_TAG=${BUILD_TAG}" >> .env
-echo "REACT_APP_BUILD_DISPLAY_NAME=${BUILD_DISPLAY_NAME}" >> .env
-echo "REACT_APP_BUILD_URL=${BUILD_URL}" >> .env
-echo "REACT_APP_BUILD_DATE=`env TZ=Europe/Berlin date`" >> .env
-echo "REACT_APP_BRANCH_NAME=${BRANCH_NAME}" >> .env
-echo "REACT_APP_GIT_COMMIT=${GIT_COMMIT}" >> .env
